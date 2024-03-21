5f18d163c4206a7916fe61970732772c68719880
2illd2 <k.e.andlou@gmail.com>
2024-03-21 23:05:12 +0300

    fj

---
+++ DSZOO.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ZOOBD.zoomagDataSetTableAdapters;
+
+namespace ZOOBD
+{
+    public partial class DSZOO : Window
+    {
+        AllDataWithoutIDsTableAdapter animals = new AllDataWithoutIDsTableAdapter();
+        public DSZOO()
+        {
+            InitializeComponent();
+            AnimalDgr.ItemsSource = animals.GetData();
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZOOBD
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void EFButton_Click(object sender, RoutedEventArgs e)
+        {
+            EFZOO productsWindow = new EFZOO(); productsWindow.ShowDialog();
+        }
+        private void DSButton_Click(object sender, RoutedEventArgs e)
+        {
+            DSZOO categoriesWindow = new DSZOO(); categoriesWindow.ShowDialog();
+        }
+    }
+}
